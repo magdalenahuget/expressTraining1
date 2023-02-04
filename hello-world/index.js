ffed9b0bc234fd6200d30ec88841d840dfe5a475
magdalenahuget <magdalena.huget@gmail.com>
2023-02-04 22:24:09 +0100

    Add routers.

--- hello-world/index.js
+++ hello-world/index.js
@@ -23,3 +23,17 @@ app.post('/hello', function(req, res){
 });
 
 app.listen(3000);
+
+
+
+// Routers
+
+var express = require('Express');
+var app = express();
+
+var things = require('./things.js');
+
+//both index.js and things.js should be in same directory
+app.use('/things', things);
+
+app.listen(3001);
